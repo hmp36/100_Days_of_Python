6823fa3008b70c23fa2359d2231c8b0559c11afe
Hagan <haganpratt@gmail.com>
2025-06-15 00:53:38 -0400

    Add initial files

---
+++ testing.py
@@ -0,0 +1,8 @@
+# Simple Python script to greet the user
+print("Hello, World!")
+
+# Ask for the user's name
+name = input("What's your name? ")
+
+# Personalized greeting
+print(f"Nice to meet you, {name}!")
