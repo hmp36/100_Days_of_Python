482393381753ae7f2b8236341e69a10e0c5926e8
Hagan <haganpratt@gmail.com>
2025-06-18 12:22:14 -0400

    Python-Flask iterations

--- Python_Flask.py
+++ Python_Flask.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask!"
+     return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "This is the About page."
+    return render_template("about.html")
 
 @app.route("/hello")
 def hello():
@@ -17,6 +17,14 @@ def hello():
 @app.route("/user/<name>")
 def user(name):
     return f"Hello, {name}!"
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
